11f7e43ee2f06ab6c832a18d24e4bff54f18d774
Alexandre <abatistadeoliveira@unomaha.edu>
2021-04-13 18:28:26 -0500

    make it better

--- efs_rest/efs_rest/wsgi.py
+++ efs_rest/efs_rest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'efs_rest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'efs_rest.efs_rest.settings')
 
 application = get_wsgi_application()
